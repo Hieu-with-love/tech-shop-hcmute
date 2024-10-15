54395673c9ed6daba1017605f7f1e3ec45c97658
hieuzeus <hieu01bdvn@gmail.com>
2024-10-15 22:10:03 +0700

    init project tech shop using spring boot and thymeleaf engine

---
+++ src/main/java/com/hcmute/tech_shop/TechShopApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.tech_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmute/tech_shop/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.tech_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
