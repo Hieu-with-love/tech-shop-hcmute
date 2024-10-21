eb5ebfafd7d3ee123de7f96b76555fad8280bdba
hieuzeus <hieu01bdvn@gmail.com>
2024-10-21 13:38:22 +0700

    config | turn off sql, jpa, spring security libraries,..., run don't need to sql connection

--- src/main/java/com/hcmute/tech_shop/controllers/LoginController.java
+++ src/main/java/com/hcmute/tech_shop/controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.hcmute.tech_shop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
 }
