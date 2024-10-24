5722c5dcd685ce4715fe5bc87d5307dfc8ead2d0
hieuzeus <hieu01bdvn@gmail.com>
2024-10-24 09:28:42 +0700

    configuration | add DTO Classes

---
+++ src/main/java/com/hcmute/tech_shop/configurations/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.hcmute.tech_shop.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/hcmute/tech_shop/dtos/requests/RatingDTO.java
@@ -0,0 +1,4 @@
+package com.hcmute.tech_shop.dtos.requests;
+
+public class RatingDTO {
+}
