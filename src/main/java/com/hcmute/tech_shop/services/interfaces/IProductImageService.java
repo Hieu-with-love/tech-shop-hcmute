3e6205d4c523c8f675003b5c1a53ed1c5d4f148e
loh <nguyenhahongtuan@gmail.com>
2024-11-02 03:37:33 +0700

    feat | CRUD Product Images

--- src/main/java/com/hcmute/tech_shop/services/interfaces/IProductImageService.java
+++ src/main/java/com/hcmute/tech_shop/services/interfaces/IProductImageService.java
@@ -1,4 +1,21 @@
 package com.hcmute.tech_shop.services.interfaces;
 
+import com.hcmute.tech_shop.entities.Product;
+import com.hcmute.tech_shop.entities.ProductImage;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 public interface IProductImageService {
+    boolean createProductImages(Long productId, MultipartFile file) throws IOException;
+
+    List<ProductImage> findByProductId(Long productId);
+
+    void deleteById(Integer integer);
+
+    Optional<ProductImage> findById(Integer integer);
+
+    Optional<Product> findById(Long aLong);
 }
