c0bbb1b4bf1668cff5cb988906ac96436d9fd15c
hieuzeus <hieu01bdvn@gmail.com>
2024-12-03 10:43:16 +0700

    address feature | addition for info template

--- src/main/java/com/hcmute/tech_shop/controllers/shipper/ShipperOperation.java
+++ src/main/java/com/hcmute/tech_shop/controllers/shipper/ShipperOperation.java
@@ -1,12 +1,22 @@
 package com.hcmute.tech_shop.controllers.shipper;
 
+import com.hcmute.tech_shop.entities.Address;
+import com.hcmute.tech_shop.services.interfaces.IAddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 @Controller
 @RequestMapping("/shipper")
+@RequiredArgsConstructor
 public class ShipperOperation {
+    private final IAddressService addressService;
 
     @GetMapping("/list-need-ship")
     public String showListShipping(){
@@ -33,8 +43,22 @@ public class ShipperOperation {
     }
 
     @GetMapping("/shipper-info")
-    public String showInfo(){
+    public String showInfo(Model model){
+        List<Address> addressList = addressService.findAll();
+        var address = addressList.stream()
+                .findFirst()
+                .orElse(new Address());
+        String detailAddress = "Ho Chi Minh City";
+        if (address.getId() != null){
+            detailAddress = address.getDetailLocation() + " " + address.getDistrict() + " " + address.getCity();
+        }
 
+        model.addAttribute("address", detailAddress);
         return "shipper/infoShipper";
+    }
+
+    @PostMapping("/update-avatar")
+    public ResponseEntity<?> updateAvatar(){
+        return ResponseEntity.ok("Successfully!");
     }
 }
